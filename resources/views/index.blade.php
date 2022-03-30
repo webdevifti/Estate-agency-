53efea5f287f58aff9aa7679ddc2fb5123592285
webdevifti <eftialam@hotmail.com>
2022-03-30 12:48:17 +0600

    o

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -95,7 +95,7 @@
     </section><!-- End Services Section -->
 
     <!-- ======= Latest Properties Section ======= -->
-    @if($get_property->count() > 0)
+   
       <section class="section-property section-t8">
         <div class="container">
           <div class="row">
@@ -120,7 +120,7 @@
               <div class="carousel-item-b swiper-slide">
                 <div class="card-box-a card-shadow">
                   <div class="img-box-a">
-                    <img src="{{ asset('frontend/assets/img/property-6.jpg') }}" alt="" class="img-a img-fluid">
+                    <img src="{{ asset('uploads/properties/thumbnails/'.$p->property_thumbnail) }}" alt="" class="img-a img-fluid">
                   </div>
                   <div class="card-overlay">
                     <div class="card-overlay-a-content">
@@ -164,158 +164,12 @@
                   </div>
                 </div>
               </div><!-- End carousel item -->
-              @endforeach
-{{--          
-              <div class="carousel-item-b swiper-slide">
-                <div class="card-box-a card-shadow">
-                  <div class="img-box-a">
-                    <img src="{{ asset('frontend/assets/img/property-3.jpg') }}" alt="" class="img-a img-fluid">
-                  </div>
-                  <div class="card-overlay">
-                    <div class="card-overlay-a-content">
-                      <div class="card-header-a">
-                        <h2 class="card-title-a">
-                          <a href="property-single.html">157 West
-                            <br /> Central Park</a>
-                        </h2>
-                      </div>
-                      <div class="card-body-a">
-                        <div class="price-box d-flex">
-                          <span class="price-a">rent | $ 12.000</span>
-                        </div>
-                        <a href="property-single.html" class="link-a">Click here to view
-                          <span class="bi bi-chevron-right"></span>
-                        </a>
-                      </div>
-                      <div class="card-footer-a">
-                        <ul class="card-info d-flex justify-content-around">
-                          <li>
-                            <h4 class="card-info-title">Area</h4>
-                            <span>340m
-                              <sup>2</sup>
-                            </span>
-                          </li>
-                          <li>
-                            <h4 class="card-info-title">Beds</h4>
-                            <span>2</span>
-                          </li>
-                          <li>
-                            <h4 class="card-info-title">Baths</h4>
-                            <span>4</span>
-                          </li>
-                          <li>
-                            <h4 class="card-info-title">Garages</h4>
-                            <span>1</span>
-                          </li>
-                        </ul>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div><!-- End carousel item -->
-
-              <div class="carousel-item-b swiper-slide">
-                <div class="card-box-a card-shadow">
-                  <div class="img-box-a">
-                    <img src="{{ asset('frontend/assets/img/property-7.jpg') }}" alt="" class="img-a img-fluid">
-                  </div>
-                  <div class="card-overlay">
-                    <div class="card-overlay-a-content">
-                      <div class="card-header-a">
-                        <h2 class="card-title-a">
-                          <a href="property-single.html">245 Azabu
-                            <br /> Nishi Park let</a>
-                        </h2>
-                      </div>
-                      <div class="card-body-a">
-                        <div class="price-box d-flex">
-                          <span class="price-a">rent | $ 12.000</span>
-                        </div>
-                        <a href="property-single.html" class="link-a">Click here to view
-                          <span class="bi bi-chevron-right"></span>
-                        </a>
-                      </div>
-                      <div class="card-footer-a">
-                        <ul class="card-info d-flex justify-content-around">
-                          <li>
-                            <h4 class="card-info-title">Area</h4>
-                            <span>340m
-                              <sup>2</sup>
-                            </span>
-                          </li>
-                          <li>
-                            <h4 class="card-info-title">Beds</h4>
-                            <span>2</span>
-                          </li>
-                          <li>
-                            <h4 class="card-info-title">Baths</h4>
-                            <span>4</span>
-                          </li>
-                          <li>
-                            <h4 class="card-info-title">Garages</h4>
-                            <span>1</span>
-                          </li>
-                        </ul>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div><!-- End carousel item -->
-
-              <div class="carousel-item-b swiper-slide">
-                <div class="card-box-a card-shadow">
-                  <div class="img-box-a">
-                    <img src="{{ asset('frontend/assets/img/property-10.jpg') }}" alt="" class="img-a img-fluid">
-                  </div>
-                  <div class="card-overlay">
-                    <div class="card-overlay-a-content">
-                      <div class="card-header-a">
-                        <h2 class="card-title-a">
-                          <a href="property-single.html">204 Montal
-                            <br /> South Bela Two</a>
-                        </h2>
-                      </div>
-                      <div class="card-body-a">
-                        <div class="price-box d-flex">
-                          <span class="price-a">rent | $ 12.000</span>
-                        </div>
-                        <a href="property-single.html" class="link-a">Click here to view
-                          <span class="bi bi-chevron-right"></span>
-                        </a>
-                      </div>
-                      <div class="card-footer-a">
-                        <ul class="card-info d-flex justify-content-around">
-                          <li>
-                            <h4 class="card-info-title">Area</h4>
-                            <span>340m
-                              <sup>2</sup>
-                            </span>
-                          </li>
-                          <li>
-                            <h4 class="card-info-title">Beds</h4>
-                            <span>2</span>
-                          </li>
-                          <li>
-                            <h4 class="card-info-title">Baths</h4>
-                            <span>4</span>
-                          </li>
-                          <li>
-                            <h4 class="card-info-title">Garages</h4>
-                            <span>1</span>
-                          </li>
-                        </ul>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div><!-- End carousel item --> --}}
+              @endforeach         
             </div>
           </div>
-          <div class="propery-carousel-pagination carousel-pagination"></div>
-
+          <div class="property-carousel-pagination carousel-pagination"></div>
         </div>
       </section><!-- End Latest Properties Section -->
-    @endif
     <!-- ======= Agents Section ======= -->
     <section class="section-agents section-t8">
       <div class="container">
